940fee7f917d7478779fe569e16489a271c14b32
Akashjayan1999 <akashjayan999@gmail.com>
2025-12-22 07:13:06 +0000

    add initalize config test

--- stablecoin/tests/stablecoin.ts
+++ stablecoin/tests/stablecoin.ts
@@ -1,16 +1,38 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Stablecoin } from "../target/types/stablecoin";
+import { PythSolanaReceiver } from "@pythnetwork/pyth-solana-receiver";
 
 describe("stablecoin", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  const connection = provider.connection;
+  const wallet = provider.wallet as anchor.Wallet;
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.stablecoin as Program<Stablecoin>;
+  const program = anchor.workspace.Stablecoin as Program<Stablecoin>;
+
+  const pythSolanaReceiver = new PythSolanaReceiver({ connection, wallet });
+  const SOL_PRICE_FEED_ID =
+    "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
+  const solUsdPriceFeedAccount = pythSolanaReceiver
+    .getPriceFeedAccountAddress(0, SOL_PRICE_FEED_ID)
+    .toBase58();
+
+  console.log(solUsdPriceFeedAccount);
+
+  const [collateralAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("collateral"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+
+
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initializeConfig()
+      .accounts({})
+      .rpc({ skipPreflight: true, commitment: "confirmed" });
     console.log("Your transaction signature", tx);
   });
 });
